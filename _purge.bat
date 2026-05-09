b4f6bd8c96b7cd662f6bc6f1683fb67c89ca3601
eidbinsaeed <eidbinsaeed@gmail.com>
2026-05-09 19:25:37 +0400

    chore: ignore local _*.bat automation helpers

--- _purge.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\eidbi\Projects\strow-ops
-if exist .git\index.lock del .git\index.lock
-if exist _commit.bat del _commit.bat
-if exist _cleanup.bat del _cleanup.bat
-git add -A
-git commit -m "chore: drop helper bats"
-git push
