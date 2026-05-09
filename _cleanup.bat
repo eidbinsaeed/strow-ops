07bcb789846d7054ba468dd228e36f51ba798077
eidbinsaeed <eidbinsaeed@gmail.com>
2026-05-09 19:25:19 +0400

    chore: drop helper bats

--- _cleanup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\eidbi\Projects\strow-ops
-if exist .git\index.lock del .git\index.lock
-del _commit.bat
-git add -A
-git commit -m "chore: drop _commit.bat helper"
-git push
-del _cleanup.bat
